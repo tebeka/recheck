582ba0c533591883090c46bfa44cff6801c02873
Miki Tebeka <miki.tebeka@gmail.com>
2020-10-31 11:20:42 +0200

    comment on go.mod & go.sum changed

--- regression_test.go
+++ regression_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 )
 
+// FIXME: go.mod & go.test are changed after running the test
+
 func TestOK(t *testing.T) {
 	cmd := exec.Command("go", "run", "./cmd/recheck", "testdata/ok.go")
 	if err := cmd.Run(); err != nil {
